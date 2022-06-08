ec529d08e2cb687fd8e8a2e6247d293bab0eb82a
brunomaria71 <brunomaria71@yahoo.com>
2022-06-08 16:01:30 -0400

    add

--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -1,3 +1,8 @@
+using Microsoft.Extensions.Options;
+using Models;
+using MongoDB.Driver;
+using Settings;
+
 using Models;
 namespace Services;
 
@@ -10,6 +15,16 @@ public interface ITodoItemService
     Task<TodoItem> CreateTodoItem(TodoItem todoItem);
 }
 public class TodoItemService : ITodoItemService
+{
+    private readonly IMongoCollection<TodoItem> todoItemCollection;
+
+    public TodoItemService(IOptions<TodoDbSettings> TodoDbSettings)
+    {
+        var client = new MongoClient(todoDbSettings.Value.ConnectionString);
+        var db = client.GetDatabase(todoDbSettings.Value.DatabaseName);
+        todoItemCollection = db.GetCollection<TodoItem>(todoDbSettings.Value.CollectionName);
+    }
+}
 
 {
     public Task<TodoItem> CreateTodoItem(TodoItem todoItem)
@@ -19,7 +34,8 @@ public class TodoItemService : ITodoItemService
 
     public Task<IEnumerable<TodoItemService>> GetAllTodoItems()
     {
-        throw new NotImplementedException();
+        var res = await todoItemCollection.FindAsync(x => true);
+        return res.ToList();
     }
 
     public Task<TodoItem> GetTodoItemById(string id)
